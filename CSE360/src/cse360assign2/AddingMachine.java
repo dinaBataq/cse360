3d1f85b34ed228a5e3fb75c60757b2fdcc373d78
dinaBataq <51976442+dinaBataq@users.noreply.github.com>
2019-06-18 23:27:31 -0700

    Adding the Javadoc comments to the implemented methods

--- CSE360/src/cse360assign2/AddingMachine.java
+++ CSE360/src/cse360assign2/AddingMachine.java
@@ -1,29 +1,58 @@
 package cse360assign2;
+/**
+ * 
+ * @author dina bataq
+ *
+ */
 
 public class AddingMachine {
 	private int total;
-	
+	private String history;
+	/**
+	 * 
+	 */
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
 	}
-	
+	/**
+	 * 
+	 * @return
+	 */
 	public int getTotal () {
-		return 0;
+		return total;
 	}
-	
-	public void add (int value) {
+	/**
+	 * 
+	 * @param value
+	 */
+	public void add (int value) 
+	{
+		total = total + value;
+		history = String.format("%s + %d", history, value);
+				
+	}
+	/**
+	 * 
+	 * @param value
+	 */
+	public void subtract (int value) 
+	{
+		total = total - value;
+		history = String.format("%s - %d", history, value);
+	}
+	/**
+	 * 	
+	 */
+	public String toString () 
+	{
+		return history;
+	}
+	/**
+	 * 
+	 */
+	public void clear()
+	{
 		
-	}
-	
-	public void subtract (int value) {
-		
-	}
-		
-	public String toString () {
-		return "";
-	}
-
-	public void clear() {
 	
 	}
 
